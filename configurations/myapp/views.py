30a2879e47ec9a973c6fb8eac610e9ecdd90fee2
salome mskhiladze <salomemskhiladze@yahoo.com>
2023-06-15 11:54:27 +0400

    add delete method

--- configurations/myapp/views.py
+++ configurations/myapp/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Car, Animal
 from .serializers import CarSerializer, AnimalSerializer
 
@@ -7,7 +9,19 @@ class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
+    @action(detail=True, methods=['delete'])
+    def delete_car(self, request, pk=None):
+        car = self.get_object()
+        car.delete()
+        return Response(status=204)
+
 
 class AnimalViewSet(viewsets.ModelViewSet):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+
+    @action(detail=True, methods=['delete'])
+    def delete_animal(self, request, pk=None):
+        animal = self.get_object()
+        animal.delete()
+        return Response(status=204)
